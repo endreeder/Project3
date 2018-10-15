172c26b46fb82bc79498ec72aa6e0c6e0615bc51
endreeder <43384274+endreeder@users.noreply.github.com>
2018-10-15 17:59:29 -0500

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,32 +1,31 @@
 from urllib.request import urlretrieve
 import re
-
 url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 lf = 'local.log'
 
 local_file, headers = urlretrieve(url, lf)
 
 regex = re.compile(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*")
-
-
-
+open('local.log').read(64)
 count=0
 for line in open('local.log'):
   count+=1
-  
 print(count)
 things = {}
-
-for line in open('local.org'):
-  pieces = re.split(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*", line)
-  filename = pieces[3]
-  if filename in things:
-    things[filename] += 1
-  else:
-    things[filename] = 1
-    
-    
+errors=[]
+for line in open('local.log'):
+        pieces = regex.split(line)
+        if len(pieces) > 7:
+                filename = pieces[4]
+                if filename in things:
+                        things[filename] += 1
+                else:
+                        things[filename] = 1
 print(things)
+
+
+
+
   
   # NOTE MOVE THIS TO THE BOTTOM
 print('Question 1: How many total requests were made in the time period represented in the log?')
